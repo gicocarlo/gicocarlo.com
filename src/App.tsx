153705df261889a91e7de90968afcb7ff8b3ac5a
gicocarlo <evangelistagico@gmail.com>
2025-06-18 07:37:50 -0700

    feat: finally getting started on website

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Footer from './layouts/Footer/Footer'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const dateObj = new Date()
+  const yearStarted = 2021
+  const yearsWorked = dateObj.getFullYear() - yearStarted
+    
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Gico Carlo Evangelista</h1>
+      <h2>
+        Software Engineer for over {yearsWorked} years. 
+      </h2>
       <Footer/>
     </>
   )

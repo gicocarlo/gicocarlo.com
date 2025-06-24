8b2a7a2f04e87f92ef3af3d99bd42bb88217067b
gicocarlo <evangelistagico@gmail.com>
2025-06-23 20:36:37 -0500

    feat: got seconds counter to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
+import { useState, useEffect } from 'react'
 import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
+  const timeStarted = new Date('2021-07-04T14:00:00.000Z').getTime()
+  const timeNow = new Date().getTime()
+  const [timePassed, setTimePassed] = useState(timeNow - timeStarted)
 
-  const dateObj = new Date()
-  const yearStarted = 2021
-  const yearsWorked = dateObj.getFullYear() - yearStarted
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimePassed(time => time + 1)
+    }, 1000)
+
+    // Cancels a timed, repeating action which was previously established 
+    // by a call to setInterval()
+    return () => clearInterval(interval);
+  }, [])
+
     
   return (
     <>
       <h1>Gico Carlo Evangelista</h1>
       <h2>
-        Software Engineer for over {yearsWorked} years. 
+        Software Engineer for over {timePassed} seconds. 
       </h2>
       <Footer/>
     </>

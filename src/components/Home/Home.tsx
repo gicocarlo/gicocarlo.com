9612fb91da98ee65fdab5230be4551e4b8e4644a
gicocarlo <evangelistagico@gmail.com>
2025-07-14 13:47:47 -0700

    feat: start about page

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import './Home.css'
+
+function Home() {
+  const dateObj = new Date()
+  const dateTimeStarted = new Date('2021-07-05T09:00:00')
+  const timeStarted = dateTimeStarted.getTime()
+  const timeNow = dateObj.getTime()
+  const [secondsPassed, setSecondsPassed] = useState(Math.floor((timeNow-timeStarted)/1000))
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecondsPassed(time => time + 1)
+    }, 1000)
+
+    // Cancels a timed, repeating action which was previously established 
+    // by a call to setInterval()
+    return () => clearInterval(interval);
+  }, [])
+
+  function convertSecondsToDate(seconds: number) {
+    let minutes = Math.floor(seconds / 60)
+    let hours = Math.floor(minutes / 60)
+    let days = Math.floor(hours / 24)
+    let years = Math.floor(days / (365.25))
+
+    let daysSinceLastYear = Math.floor(days % 365 - 1)
+    let hoursPassed = dateObj.getHours() < 9 ? (dateObj.getHours() + 24) - 9 : dateObj.getHours() - 9
+
+    return (
+      <>
+        <h3>{years} years</h3>
+        <h3>{daysSinceLastYear} days</h3>
+        <h3>{hoursPassed} hours</h3>
+        <h3>{dateObj.getMinutes()} minutes</h3>
+        <h3>{dateObj.getSeconds()} seconds</h3>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>Gico Carlo Evangelista</h1>
+      <h2>
+        Software Engineer for over:
+      </h2>
+      {convertSecondsToDate(secondsPassed)}
+    </>
+  )
+}
+
+export default Home
